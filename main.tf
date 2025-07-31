b014204293b8162517c600106bd55d53397f201f
ramashashank2003 <ramashashank2003@gmail.com>
2025-07-31 12:47:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-region = "ap-south-1"
+region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
 count = 5
-ami = "ami-0f918f7e67a3323f0"
+ami = "ami-020cba7c55df1f615"
 instance_type = "t2.medium"
-key_name = "RMUMBAIKP"
-vpc_security_group_ids = ["sg-0da91ed9997d755fc"]
+key_name = "project"
+vpc_security_group_ids = ["sg-0b252a39f1b4c4475"]
 tags = {
 Name = var.instance_names[count.index]
 }
